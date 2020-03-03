f0b58adfbcb49c3086ffb6935fa6335c1907f68d
Ulises Gomez <ulisesgomez120@gmail.com>
2020-03-02 20:05:43 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { LandingComponent } from "./components/landing/landing.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { OverviewComponent } from './components/dashboard/overview/overview.component';
+import { ExpensesComponent } from './components/dashboard/expenses/expenses.component';
+import { SavingsComponent } from './components/dashboard/savings/savings.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, LandingComponent, DashboardComponent, OverviewComponent, ExpensesComponent, SavingsComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
